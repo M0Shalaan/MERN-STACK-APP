b8d8a1d317bf6a5a61cce3c2859c6c9fa8b4baf4
M0Shalaan <58323465+M0Shalaan@users.noreply.github.com>
2023-01-19 23:24:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/WorkoutDetails.js
@@ -0,0 +1,41 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { FaTrashAlt } from "react-icons/fa";
+
+// date fns
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Number of reps: </strong>
+        {workout.reps}
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      {/* delete icon */}
+
+      <span onClick={handleClick}>
+        <FaTrashAlt />
+      </span>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
