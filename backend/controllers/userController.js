020d2f54e67fa12b128588473eb78635b7c67128
z8man123@gmail.com <mohamedshalaan67@gmail.com>
2023-06-09 14:56:30 +0300

    added jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+// creating token
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login a user
 const loginUser = async (req, res) => {
@@ -11,13 +17,12 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    res.status(200).json({ email, user });
-
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
-
   }
-  
 };
 
 module.exports = { signupUser, loginUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,23 +1,34 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
     /*unique is for not to 
     add an already registered user. */
   },
 
   password: {
     type: String,
-    required: true
+    required: true,
   },
 });
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled!");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
   const exists = await this.findOne({ email });
   if (exists) {
     throw Error("Email already in use");
