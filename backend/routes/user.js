33f3e8048d021a08951bc0949b3147bd8a4abc7f
z8man123@gmail.com <mohamedshalaan67@gmail.com>
2023-06-09 10:17:12 +0300

    added user file fo auth

---
+++ backend/routes/user.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { route } = require("./workouts");
+
+const router = express.Router;
+
+// login route
+
+router.post("/login", () => {});
+
+// signup route
+router.post("/signup", () => {});
+
+
+
+module.exports = router;
