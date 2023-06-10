9c2faf51495d03fe5ccb463751b72deece58bdf1
z8man123@gmail.com <mohamedshalaan67@gmail.com>
2023-06-10 11:15:50 +0300

    added some files

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,6 +17,7 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
 // static signup method
 userSchema.statics.signup = async function (email, password) {
   // validation
@@ -36,6 +37,23 @@ userSchema.statics.signup = async function (email, password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
   const user = await this.create({ email, password: hash });
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled!");
+  }
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Invalid password");
+  }
   return user;
 };
 
