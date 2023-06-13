1719aa8e21b5474d16e7fbe6caf2b739242460da
Mohamed Shalaan <z8man123@gmail.com>
2023-06-13 21:28:20 +0300

    some changes

--- FrontEnd/src/hooks/useLogout.js
+++ FrontEnd/src/hooks/useLogout.js
@@ -1,12 +1,17 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
+
 export const useLogout = () => {
-  const { dispach } = useAuthContext();
+  const { dispatch } = useAuthContext();
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext();
+
   const logout = () => {
-    //   remove user from local storage
+    // remove user from storage
     localStorage.removeItem("user");
 
     // dispatch logout action
-    dispach({ type: "LOGOUT" });
+    dispatch({ type: "LOGOUT" });
+    dispatchWorkouts({ type: "SET_WORKOUTS", payload: null });
   };
 
   return { logout };
